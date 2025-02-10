ba9355433686a5d6c73231c9a8f36026d7dac527
sergiopoli77 <S22210048@student.unklab.ac.id>
2025-02-10 09:15:29 +0800

    add pertemuan 10 februari express js

--- index.js
+++ index.js
@@ -1,52 +1,13 @@
 const http = require("http");
+const { hello, greetings } = require("./helloWorld");
 const moment = require("moment");
-const users = require("./users");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/json");
-    res.write(
-      JSON.stringify({
-        status: "success",
-        message: "This the homepage",
-      })
-    );
-  } else if (url === "/about") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/json");
-    res.write(
-      JSON.stringify({
-        status: "success",
-        message: "Response success",
-        description: "Exercise #02",
-        date: moment().format(),
-      })
-    );
-  } else if (url === "/users") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/json");
-    res.write(
-      JSON.stringify({
-        users,
-      })
-    );
-  } else {
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/json");
-    res.write(
-      JSON.stringify({
-        status: "not found",
-        message: "Route tidak ditemukan",
-      })
-    );
-  }
-  res.end();
-});
+app.get("/", (req, res) => res.send("Hello World"));
 
 const hostname = "127.0.0.1";
 const port = 3000;
-server.listen(port, hostname, () =>
+app.listen(port, hostname, () =>
   console.log(`Server running at http://${hostname}:${port}`)
 );
