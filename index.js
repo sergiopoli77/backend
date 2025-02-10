61384289b5d21bcbcafd46d4bc16b7c3ee516d7a
sergiopoli77 <S22210048@student.unklab.ac.id>
2025-02-10 09:20:30 +0800

    add json

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello World"));
+app.get("/about", (req, res) =>
+  res.status(200).json({
+    status: "success",
+    message: "about page",
+    data: [],
+  })
+);
 
 const hostname = "127.0.0.1";
 const port = 3000;
