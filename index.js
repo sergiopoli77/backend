58229d6c298124df6e42aa3e2650405776f013ee
sergiopoli77 <S22210048@student.unklab.ac.id>
2025-02-12 09:10:49 +0800

    add nodemon

--- index.js
+++ index.js
@@ -8,27 +8,23 @@ app.get("/", (req, res) => res.send("Hello World"));
 app.get("/about", (req, res) =>
   res.status(200).json({
     status: "success",
-    message: "about page",
+    message: "About page",
     data: [],
   })
 );
-
-//method http
 app.post("/contoh", (req, res) => res.send("request method POST"));
-app.put("/contoh", (req, res) => res.send("request method PUT"));
-app.delete("/contoh", (req, res) => res.send("request method DELETE"));
-app.patch("/contoh", (req, res) => res.send("request method PATCH"));
-app.all("/universal", (req, res) =>
-  res.send(`request dengan method ${req.method}`)
-);
+app.put("/contoh", (req, res) => res.send("Request method PUT"));
+app.delete("/contoh", (req, res) => res.send("Request method DELETE"));
+app.patch("/contoh", (req, res) => res.send("Request method PATCH"));
 
-// 1. Routing dinamis menggunakan params
-app.get("/post/:id", (req, res) => res.send(`Artikel ke - $ {req.params.id}`));
-
+app.all("/universal", (req, res) => res.send(`Request method ${req.method}`));
+// Routing dinamis
+// 1. Menggunakan params
+app.get("/post/:id", (req, res) => res.send(`Artikel ke - ${req.params.id}`));
 // 2. Menggunakan query string
-app.get("post", (req, res) => {
-  const { page } = req.query;
-  res.send(`Query yang didapatkan adalah : ${page}`);
+app.get("/post", (req, res) => {
+  const { page, sort } = req.query;
+  res.send(`Query yang didapatkan adalah : ${page}, sort : ${sort}`);
 });
 
 const hostname = "127.0.0.1";
