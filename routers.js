ea82a08de4a5a84f80823596263fb1647e8cb5c0
sergiopoli77 <S22210048@student.unklab.ac.id>
2025-02-26 09:28:11 +0800

    add can download image

--- routers.js
+++ routers.js
@@ -18,7 +18,11 @@ const path = require("path");
 
 routers.get("/download", (req, res) => {
   const filename = "dummy.png";
-  res.sendFile(path.join(__dirname, "/donwload", filename));
+  res.sendFile(path.join(__dirname, "/donwload", filename), {
+    headers: {
+      "Content-Disposition": 'attachment; filename="dummy-photo.png',
+    },
+  });
 });
 
 routers.post("/login", (req, res) => {
