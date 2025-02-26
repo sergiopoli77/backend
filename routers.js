17579dcd5d55229b9bce94f164b1ec1362441704
sergiopoli77 <S22210048@student.unklab.ac.id>
2025-02-26 09:20:50 +0800

    add download

--- routers.js
+++ routers.js
@@ -2,6 +2,11 @@ const express = require("express");
 const routers = express.Router();
 
 // Routing
+routers.get("/download", (req, res) => {
+  const filename = "dummy.png";
+  res.sendFile(__dirname + "/download/" + filename);
+});
+
 routers.post("/login", (req, res) => {
   const { username, password } = req.body;
   res.status(200).json({
