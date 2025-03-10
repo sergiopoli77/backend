8941199573f7263c54250f7fd2406e43e69dc701
sergiopoli77 <S22210048@student.unklab.ac.id>
2025-03-10 09:35:05 +0800

    add exercise5

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
-const http = require("http");
-const { hello, greetings } = require("./helloWorld");
-const moment = require("moment");
 const express = require("express");
 const morgan = require("morgan");
-// const errorhandler = require("errorhandler");
-const app = express();
-const routers = require("./routers");
 const path = require("path");
 const cors = require("cors");
+const routers = require("./routers");
 
-//Middleware
-const log = (req, res, next) => {
-  console.log(
-    moment().format("h:mm:ss a") + " " + req.originalUrl + " " + req.ip
-  );
-  next();
-};
+const app = express();
 
+// Middleware
 app.use(morgan("tiny"));
-// app.use(errorhandler);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
     origin: "http://127.0.0.1:5500",
-    methods: ["GET", "PUT"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
-//Routing
+// Routing
 app.use(routers);
 
-//Middleware untuk 404
-app.use((req, res, next) => {
+// Middleware untuk 404
+app.use((req, res) => {
   res.status(404).json({
     status: "error",
     message: "resource tidak ditemukan",
   });
 });
 
-const hostname = "127.0.0.1";
-const port = 3000;
-app.listen(port, hostname, () =>
-  console.log(`Server running at http://${hostname}:${port}`)
-);
+// Middleware untuk error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    status: "error",
+    message: "terjadi kesalahan pada server",
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://127.0.0.1:${PORT}`);
+});
--- routers.js
+++ routers.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const routers = express.Router();
 const path = require("path");
 const fs = require("fs");
 const multer = require("multer");
-const upload = multer({ dest: "public" });
+const users = require("./users");
 
+const routers = express.Router();
+const upload = multer({ dest: "public/uploads" });
 
-// Routing
+// Upload file
 routers.post("/upload", upload.single("file"), (req, res) => {
   const file = req.file;
-
   if (file) {
-    const target = path.join(__dirname, "public", file.originalname);
+    const target = path.join(__dirname, "public/uploads", file.originalname);
     fs.renameSync(file.path, target);
     res.send("File berhasil diupload");
   } else {
@@ -19,70 +19,78 @@ routers.post("/upload", upload.single("file"), (req, res) => {
   }
 });
 
-//download
-// routers.get("/download", (req, res) => {
-//   const filename = "dummy.png";
-//   res.sendFile(__dirname + "/download/" + filename);
-// });
-
-//path
-//kalau di luar
-// routers.get("/download", (req, res) => {
-//   const filename = "dummy.png";
-//   res.sendFile(path.join(__dirname), filename);
-// });
-
-// routers.get("/download", (req, res) => {
-//   const filename = "dummy.png";
-//   res.sendFile(path.join(__dirname, "/download", filename), {
-//     headers: {
-//       "Content-Disposition": 'attachment; filename="dummy-photo.png',
-//     },
-//   });
-// });
-
-//cara lain
+// Download file
 routers.get("/download", (req, res) => {
   const filename = "dummy.png";
-  res.download(path.join(__dirname, "/download", filename), "dummpy-photo.png");
+  res.download(path.join(__dirname, "download", filename), "dummy-photo.png");
 });
 
-routers.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  res.status(200).json({
+// Get all users
+routers.get("/users", (req, res) => {
+  res.status(200).json({ status: "success", data: users });
+});
+
+// Get user by name (case-insensitive)
+routers.get("/users/:name", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const user = users.find((u) => u.name.toLowerCase() === name);
+  if (user) {
+    res.status(200).json({ status: "success", data: user });
+  } else {
+    res.status(404).json({ status: "error", message: "User tidak ditemukan" });
+  }
+});
+
+// Add a new user
+routers.post("/users", (req, res) => {
+  const { name, age } = req.body;
+  if (!name || !age) {
+    return res
+      .status(400)
+      .json({ status: "error", message: "Nama dan usia harus diisi" });
+  }
+  users.push({ name, age });
+  res.status(201).json({
     status: "success",
-    message: "Login Page",
-    data: {
-      username: username,
-      password: password,
-    },
+    message: "User berhasil ditambahkan",
+    data: { name, age },
   });
 });
-routers.get("/", (req, res) => res.send("Hello World"));
-routers.get("/about", (req, res) =>
+
+// Update user by name
+routers.put("/users/:name", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const { age } = req.body;
+  const userIndex = users.findIndex((u) => u.name.toLowerCase() === name);
+  if (userIndex === -1) {
+    return res
+      .status(404)
+      .json({ status: "error", message: "User tidak ditemukan" });
+  }
+  if (!age) {
+    return res
+      .status(400)
+      .json({ status: "error", message: "Usia harus diisi" });
+  }
+  users[userIndex].age = age;
   res.status(200).json({
     status: "success",
-    message: "About page",
-    data: [],
-  })
-);
-routers.post("/contoh", (req, res) => res.send("request method POST"));
-routers.put("/contoh", (req, res) => res.send("Request method PUT"));
-routers.delete("/contoh", (req, res) => res.send("Request method DELETE"));
-routers.patch("/contoh", (req, res) => res.send("Request method PATCH"));
+    message: "User berhasil diperbarui",
+    data: users[userIndex],
+  });
+});
 
-routers.all("/universal", (req, res) =>
-  res.send(`Request method ${req.method}`)
-);
-// Routing dinamis
-// 1. Menggunakan params
-routers.get("/post/:id", (req, res) =>
-  res.send(`Artikel ke - ${req.params.id}`)
-);
-// 2. Menggunakan Query String
-routers.get("/post", (req, res) => {
-  const { page, sort } = req.query;
-  res.send(`Query string= page :${page}, sort : ${sort}`);
+// Delete user by name
+routers.delete("/users/:name", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const userIndex = users.findIndex((u) => u.name.toLowerCase() === name);
+  if (userIndex === -1) {
+    return res
+      .status(404)
+      .json({ status: "error", message: "User tidak ditemukan" });
+  }
+  users.splice(userIndex, 1);
+  res.status(200).json({ status: "success", message: "User berhasil dihapus" });
 });
 
 module.exports = routers;
