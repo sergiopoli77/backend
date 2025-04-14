69f6b3232383719e54b05aa6a14d30144454289f
sergiopoli77 <S22210048@student.unklab.ac.id>
2025-04-14 09:14:28 +0800

    add pertemuan 11

--- routers.js
+++ routers.js
@@ -5,7 +5,9 @@ const fs = require("fs");
 const multer = require("multer");
 const upload = multer({ dest: "public" });
 const client = require("./mongodb");
+const { ObjectId } = require("mongodb");
 
+// Get All Users
 routers.get("/users", async (req, res) => {
   try {
     const db = client.db("latihan");
@@ -15,8 +17,48 @@ routers.get("/users", async (req, res) => {
       message: "list users",
       data: users,
     });
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
 });
+
+// Get Single User
+routers.get("/users/:id", async (req, res) => {
+  try {
+    const db = client.db("latihan");
+    const user = await db.collection("users").findOne({
+      _id: new ObjectId(req.params.id),
+    });
+    if (!user) {
+      return res.status(404).json({
+        status: "fail",
+        message: "User not found",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      message: "single user",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
+
+// Insert User
+
+
+// Update User
+
+
+// Delete User
+
+// Get Order User
+
+// Upload File
 routers.post("/upload", upload.single("file"), (req, res) => {
   const file = req.file;
   if (file) {
@@ -28,28 +70,28 @@ routers.post("/upload", upload.single("file"), (req, res) => {
   }
 });
 
+// Download File
 routers.get("/download", (req, res) => {
   const filename = "/th.jpg";
   res.download(path.join(__dirname, filename), "th.jpg");
-  // res.sendFile(path.join(__dirname , filename), {
-  //   headers: {
-  //     "Content-Disposition": 'attachment; filename="th.jpg"'
-  //   }
-  // })
 });
-// Routing
+
+// Login
 routers.post("/login", (req, res) => {
   const { username, password } = req.body;
   res.status(200).json({
     status: "success",
     message: "Login page",
     data: {
-      username: username,
-      password: password,
+      username,
+      password,
     },
   });
 });
+
+// Static Routes
 routers.get("/", (req, res) => res.send("Hello World"));
+
 routers.get("/about", (req, res) =>
   res.status(200).json({
     status: "success",
@@ -65,6 +107,7 @@ routers.put("/about", (req, res) =>
     data: [],
   })
 );
+
 routers.post("/contoh", (req, res) => res.send("request method POST"));
 routers.put("/contoh", (req, res) => res.send("Request method PUT"));
 routers.delete("/contoh", (req, res) => res.send("Request method DELETE"));
@@ -73,11 +116,13 @@ routers.patch("/contoh", (req, res) => res.send("Request method PATCH"));
 routers.all("/universal", (req, res) =>
   res.send(`Request method ${req.method}`)
 );
-// Routing dinamis
+
+// Dynamic Routes
 // 1. Menggunakan params
 routers.get("/post/:id", (req, res) =>
   res.send(`Artikel ke - ${req.params.id}`)
 );
+
 // 2. Menggunakan Query String
 routers.get("/post", (req, res) => {
   const { page, sort } = req.query;
